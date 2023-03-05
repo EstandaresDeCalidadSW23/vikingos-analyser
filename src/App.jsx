6d5f1759b14ae649229a6c8b12eb7c6f15c040f9
danielderma <danielderma.contact@gmail.com>
2023-03-04 18:35:14 -0600

    fix: text to code

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,17 @@ function App() {
             className="border px-4 py-2 "
           >
             <option className="px-2" value="lexico">
-              Léxico
+              Lexical
             </option>
             <option className="px-2" value="sintactico">
-              Sintáctico
+              Syntactic
             </option>
           </select>
         </div>
-        <h4 className="mb-2 font-semibold">Text</h4>
+        <h4 className="mb-2 font-semibold">Code</h4>
         <textarea
-          className="border w-full h-80 resize-none p-2"
-          placeholder="Enter your text here"
+          className="border w-full h-80 resize-none p-2 font-mono"
+          placeholder="Enter your code here."
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
@@ -44,7 +44,7 @@ function App() {
           </button>
         </div>
         <div className="border w-full bg-gray-300 p-2 mt-4">
-          <h4 className="mb-2 text-gray-500">The result will display here.</h4>
+          <p className="mb-2 text-gray-500">The result will display here.</p>
         </div>
       </div>
     </div>
